bedf0987c28d2fdbd9f6717505e9e2f786e4a6ed
xXCHIRRIXx <juand-castroc@unilibre.edu.co>
2026-07-29 11:37:27 -0500

    Actualizar ruta de Firebase de forma segura

--- backend/auth/firebase.config.js
+++ backend/auth/firebase.config.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const { getAuth } = require('firebase-admin/auth');
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = require('../../serviceAccountKey.json');
 
 const app = initializeApp({
   credential: cert(serviceAccount)
